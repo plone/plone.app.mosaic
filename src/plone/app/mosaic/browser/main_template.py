2e1bf9b77f715c263ae547f99d00227912abee9e
Asko Soukka <asko.soukka@iki.fi>
2014-06-11 02:16:23 +0200

    Add magic to turn HTML-layouts into TAL-templates; Add missing macros from the legacy main_template

--- src/plone/app/mosaic/browser/main_template.py
+++ src/plone/app/mosaic/browser/main_template.py
@@ -1,15 +1,33 @@
+from lxml import etree
+from repoze.xmliter.utils import getHTMLSerializer
 from zope.interface import implements
-
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.memoize import view
+from zope.pagetemplate.pagetemplate import PageTemplate
 
 from plone.app.mosaic.browser.interfaces import IMainTemplate
-from plone.app.blocks.utils import getLayoutAwareSiteLayout, resolveResource
+from plone.app.blocks.utils import resolveResource
+from plone.app.blocks.utils import getDefaultSiteLayout, panelXPath
 
-from plone.app.blocks.utils import getDefaultSiteLayout
-from plone.app.blocks.resource import DefaultSiteLayout
-from zope.pagetemplate.pagetemplate import PageTemplate
+
+def main_templatize(layout):
+    result = getHTMLSerializer(layout, encoding='utf-8')
+    nsmap = {'metal': 'http://namespaces.zope.org/metal'}
+
+    # wrap all panels with a metal:fill-slot -tag
+    for layoutPanelNode in panelXPath(result.tree):
+        panelId = layoutPanelNode.attrib['data-panel']
+        slot = etree.Element('{%s}%s' % (nsmap['metal'], panelId), nsmap=nsmap)
+        slot.attrib['define-slot'] = panelId
+        slot_parent = layoutPanelNode.getparent()
+        slot_parent_index = slot_parent.index(layoutPanelNode)
+        slot.append(layoutPanelNode)
+        slot_parent.insert(slot_parent_index, slot)
+
+    template = '<metal:page define-macro="master">\n%s\n</metal:page>'
+    metal = 'xmlns:metal="http://namespaces.zope.org/metal"'
+    return (template % ''.join(result)).replace(metal, '')
 
 
 class MainTemplate(BrowserView):
@@ -30,9 +48,11 @@ class MainTemplate(BrowserView):
             layout_resource_path = getDefaultSiteLayout(self.context)
             layout = resolveResource(layout_resource_path)
             pt = PageTemplate()
-            pt.write(layout)
+            pt.write(main_templatize(layout))
             return pt
 
     @property
     def macros(self):
-        return self.template.macros
+        macros = self.main_template.macros.copy()
+        macros.update(self.template.macros.copy())
+        return macros
