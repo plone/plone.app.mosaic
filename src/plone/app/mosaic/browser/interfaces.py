3b05900cebf456fc4c5ba0438765b347a387c6fe
Asko Soukka <asko.soukka@iki.fi>
2014-06-26 12:09:13 +0300

    Fix to use CMFPlone's IMainTemplate on Plone 5

--- src/plone/app/mosaic/browser/interfaces.py
+++ src/plone/app/mosaic/browser/interfaces.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from zope.interface import Interface
 
+try:
+    from Products.CMFPlone.browser.interfaces import IMainTemplate
 
+except ImportError:
 
-class IMainTemplate(Interface):
-    """Interface to the view that generated the main_template"""
+    class IMainTemplate(Interface):
+        """Interface to the view that generated the main_template"""
+
