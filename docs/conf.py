6528fc9c56e9fdad1326266bb281ad09aeb3a218
Asko Soukka <asko.soukka@iki.fi>
2014-07-16 22:00:41 +0300

    Add custom stylesheet

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ pygments_style = 'sphinx'
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
 templates_path = ['_templates']
 
 # -- Options for LaTeX output -------------------------------------------------
