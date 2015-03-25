626a05a284cdceb0b14d11f650d7d6d7975ac03b
Asko Soukka <asko.soukka@iki.fi>
2015-03-25 09:47:02 +0200

    Update requirejs-bundle for Plone 4

---
+++ build.js
@@ -0,0 +1,23 @@
+({
+    baseUrl: 'src/plone/app/mosaic/browser/static/js',
+    name: 'mosaic.core',
+    include: [
+      'mosaic.actions',
+      'mosaic.editor',
+      'mosaic.layout',
+      'mosaic.overlay',
+      'mosaic.toolbar',
+      'mosaic.undo',
+      'mosaic.upload'
+    ],
+    out: 'src/plone/app/mosaic/browser/static/plone-mosaic.js',
+    optimize: 'uglify2',
+    generateSourceMaps: true,
+    preserveLicenseComments: false,
+    paths: {
+        'jquery': 'empty:',
+        'tinymce': 'empty:',
+        'mockup-patterns-modal': 'empty:'
+    }
+})
+
