0e6c9d41981f9210b51df28f90193bbf1f2a1502
Asko Soukka <asko.soukka@iki.fi>
2015-05-24 22:48:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import os
 
 version = '1.0a1.dev0'
 
@@ -10,46 +9,53 @@ long_description = (
     open('CHANGES.rst').read()
     + '\n')
 
-setup(name='plone.app.mosaic',
-      version=version,
-      description="Plone Mosaic main package",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='https://github.com/plone/plone.app.mosaic',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.api',
-          'plone.app.blocks',
-          'plone.app.tiles',
-          'plone.app.drafts',
-          'plone.app.standardtiles',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={'test': ['plone.app.testing',
-                               'plone.app.contenttypes',
-                               'plone.app.widgets>=1.8.0.dev0',
-                               'plone.app.robotframework']},
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    name='plone.app.mosaic',
+    version=version,
+    description='Plone Mosaic',
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.0',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='https://github.com/plone/plone.app.mosaic',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.api',
+        'plone.app.blocks',
+        'plone.app.tiles',
+        'plone.app.drafts',
+        'plone.app.standardtiles',
+    ],
+    extras_require={'test': [
+        'plone.app.testing',
+        'plone.app.contenttypes',
+        'plone.app.widgets>=1.8.0.dev0',
+        'plone.app.robotframework'
+    ]},
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
