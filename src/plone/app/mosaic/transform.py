6c84701684679ed79e8faf4dbca1473c416d9e4c
Asko Soukka <asko.soukka@iki.fi>
2015-05-31 17:43:09 +0300

    Add HTTPHeaders transform to ensure that the response headers normally set by plone.httpheaders viewlet manager are also set for HTML site layouts

---
+++ src/plone/app/mosaic/transform.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from Acquisition import aq_base
+from Acquisition import aq_parent
+from plone.transformchain.interfaces import ITransform
+from zope.component import getAdapters
+from zope.component import queryMultiAdapter
+from zope.interface import implementer
+from zope.viewlet.interfaces import IViewletManager
+from zope.viewlet.interfaces import IViewlet
+
+
+@implementer(ITransform)
+class HTTPHeaders(object):
+    """Ensure that HTTP response headers normally set when main_template
+    renders plone.httpheaders viewlet manager are also set when main_template
+    is not called (e.g. with pure-HTML site layouts).
+    """
+
+    order = 9000
+
+    def __init__(self, published, request):
+        self.published = published
+        self.request = request
+
+    def _setHeaders(self):
+        context = aq_parent(aq_base(self.published))
+        manager = queryMultiAdapter(
+            (context, self.request, self.published),
+            IViewletManager, name='plone.httpheaders'
+        )
+        if manager is not None:
+            headers = map(str.lower, self.request.response.headers)
+            for name, viewlet in getAdapters(
+                (context, self.request, self.published, manager),
+                IViewlet
+            ):
+                for key, value in viewlet.getHeaders():
+                    if key.lower() not in headers:
+                        print self.request.URL, key, value
+                        self.request.response.setHeader(key, value)
+
+    def transformString(self, result, encoding):
+        self._setHeaders()
+        return None
+
+    def transformUnicode(self, result, encoding):
+        self._setHeaders()
+        return None
+
+    def transformIterable(self, result, encoding):
+        self._setHeaders()
+        return None
