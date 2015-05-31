e64463941808c095c65dce9a5406da90a8a9edd8
Asko Soukka <asko.soukka@iki.fi>
2015-05-31 21:40:12 +0300

    Add HTMLLanguage transform to set correct langauge for HTML site layouts

--- src/plone/app/mosaic/transform.py
+++ src/plone/app/mosaic/transform.py
@@ -2,11 +2,12 @@
 from Acquisition import aq_base
 from Acquisition import aq_parent
 from plone.transformchain.interfaces import ITransform
+from repoze.xmliter.serializer import XMLSerializer
 from zope.component import getAdapters
 from zope.component import queryMultiAdapter
 from zope.interface import implementer
-from zope.viewlet.interfaces import IViewletManager
 from zope.viewlet.interfaces import IViewlet
+from zope.viewlet.interfaces import IViewletManager
 
 
 @implementer(ITransform)
@@ -53,3 +54,33 @@ class HTTPHeaders(object):
     def transformIterable(self, result, encoding):
         self._setHeaders()
         return None
+
+
+@implementer(ITransform)
+class HTMLLanguage(object):
+    """Set HTML tag language attributes"""
+
+    order = 8950
+
+    def __init__(self, published, request):
+        self.published = published
+        self.request = request
+
+    def transformString(self, result, encoding):
+        return None
+
+    def transformUnicode(self, result, encoding):
+        return None
+
+    def transformIterable(self, result, encoding):
+        if not self.request.get('plone.app.blocks.enabled', False) or \
+                not isinstance(result, XMLSerializer):
+            return None
+
+        # TODO: Any better (yet simple) way to figure out current language?
+        lang = getattr(self.request, 'LANGUAGE', 'en')
+        root = result.tree.getroot()
+        if root is not None:
+            root.attrib['lang'] = lang
+
+        return result
