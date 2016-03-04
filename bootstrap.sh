5763f8b9ddd78557a88ba8d5e3d56979955d6767
Jens W. Klein <jk@kleinundpartner.at>
2016-03-04 11:24:53 +0100

    use requirements file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
+# bootstrap using pip
 rm -r ./lib ./include ./local ./bin
 virtualenv --clear .
-./bin/pip install --upgrade pip setuptools zc.buildout
-./bin/buildout
+./bin/pip install -r requirements.txt
+./bin/buildout $*
