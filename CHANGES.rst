a923f0cece130bb77544d6f91218c2cdd6a1d0c5
Asko Soukka <asko.soukka@iki.fi>
2016-04-08 07:35:10 +0300

    Back to development: 1.0rc3

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+1.0rc3 (unreleased)
+-------------------
+
+- Nothing changed yet.
+
+
 1.0rc2 (2016-04-08)
 -------------------
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '1.0rc2'
+version = '1.0rc3.dev0'
 
 long_description = (
     open('README.rst').read()
