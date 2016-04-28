19e6e6c64279327e35365802b3d9bb5beb6597a5
Asko Soukka <asko.soukka@iki.fi>
2016-04-28 13:59:43 +0300

    Add test for layout widget registration

---
+++ src/plone/app/mosaic/tests/test_widget.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from plone.app.blocks.layoutbehavior import ILayoutAware
+from plone.app.mosaic.testing import PLONE_APP_MOSAIC_INTEGRATION
+from plone.app.mosaic.widget import LayoutWidget
+from z3c.form.interfaces import IFieldWidget
+from zope.component import getMultiAdapter
+import unittest2 as unittest
+
+
+class TestLayoutWidget(unittest.TestCase):
+    layer = PLONE_APP_MOSAIC_INTEGRATION
+
+    def setUp(self):
+        self.request = self.layer['request']
+
+    def test_layout_widget_is_registered_for_layer(self):
+        widget = getMultiAdapter((ILayoutAware['content'], self.request),
+                                  IFieldWidget)
+        self.assertIsInstance(widget, LayoutWidget)
