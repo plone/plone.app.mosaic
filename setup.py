bfaa856f968c7ac9c524567871e32ad2e8833e78
Asko Soukka <asko.soukka@iki.fi>
2016-09-15 21:08:48 +0300

    Back to development: 2.0.0rc2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '2.0.0rc1'
+version = '2.0.0rc2.dev0'
 
 long_description = (
     open('README.rst').read() + '\n' +
