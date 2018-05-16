956868d8ebf65d4259cd893e87bbe77e24493241
Peter Holzer <peter.holzer@agitator.com>
2018-05-16 15:39:50 +0200

    remove version pin for plone.app.layout

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'plone.app.drafts>=1.1.2',
         'plone.app.standardtiles>=2.2.0',
         'Products.CMFPlone>=5.0.4',
-        'plone.app.layout>=2.8.0',
+        'plone.app.layout',
     ],
     extras_require={'test': [
         'plone.app.testing',
