2a32ff12afc990bbfb73e683e7aa10f488989b52
Asko Soukka <asko.soukka@iki.fi>
2019-08-09 10:14:14 +0300

    Fix to only call tinymce editor.setContent if editor.parser has been initialized

--- src/plone/app/mosaic/browser/static/js/mosaic.tile.js
+++ src/plone/app/mosaic/browser/static/js/mosaic.tile.js
@@ -1066,7 +1066,7 @@ define([
           editor = tinymce.get(editor_id);
           var content = $('.mosaic-' + tiletype + '-tile', $.mosaic.document).find('.mosaic-tile-content').html();
           $textarea.val(content);
-          if (editor) {
+          if (editor && editor.parser) {
             editor.setContent(content);
           }
           break;
