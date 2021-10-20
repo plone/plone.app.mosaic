c47e3e6981fc7bd7391cd914d93040543279a296
Peter Mathis <peter.mathis@kombinat.at>
2021-10-20 12:30:55 +0200

    version bump ... 3.x targets Plone 6.0 only

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.2.3.dev0'
+version = '3.0.0.dev0'
 
 long_description = (
     open('README.rst').read() + '\n' +
@@ -21,13 +21,12 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Plone',
-        'Framework :: Plone :: 5.1',
-        'Framework :: Plone :: 5.2',
+        'Framework :: Plone :: 6.0',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
