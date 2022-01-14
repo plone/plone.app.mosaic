d72696b9c561fbf510c91f639eb21845c71b6bb5
Peter Holzer <peter.holzer@agitator.com>
2022-01-14 10:32:38 +0100

    major cleanup

--- resources/src/plone-mosaic/default.js
+++
@@ -1,7 +0,0 @@
-require('plone');
-
-require('./default.less');
-require('./theme.less');
-
-window.jQuery = require('jquery');  // Expose jQuery for BBB
-window.require = undefined;  // Fix @@search
--- resources/src/plone-mosaic/logged-in.js
+++
@@ -1,6 +0,0 @@
-require('plone-logged-in');
-require('resourceregistry');
-require('mosaic');
-require('layouts-editor');
-
-require('./logged-in.less');
