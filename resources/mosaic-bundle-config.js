995946cb32d41f3baa3bd1291ad2f04c79c2651e
Johannes Raggam <thetetet@gmail.com>
2022-01-14 15:11:37 +0100

    es6 - begin transforming requirejs to es6

---
+++ resources/mosaic-bundle-config.js
@@ -0,0 +1,4 @@
+import registry from "@patternslib/patternslib/src/core/registry";
+import "./js/mosaic.pattern";
+
+registry.init();
--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,13 @@ const path = require("path");
 const mockup_config = require("mockup/webpack.config.js");
 
 module.exports = async (env, argv) => {
-
     let config = {
         entry: {
-            mosaic-bundle: path.resolve(__dirname, "resources/mosaic-bundle-config"),
+            "mosaic-bundle": path.resolve(__dirname, "resources/mosaic-bundle-config"),
         },
     };
-    const config = await mockup_config(env, argv, __dirname);
-    config.output.path = path.resolve(
-        __dirname,
-        "src/plone/app/mosaic/browser/static"
-    );
+    config = await mockup_config(env, argv, __dirname);
+    config.output.path = path.resolve(__dirname, "src/plone/app/mosaic/browser/static");
 
     return config;
 };
