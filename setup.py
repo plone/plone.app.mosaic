9b9c06ac5e3a8cd99be8ee2405480432492bfa8f
Peter Mathis <peter.mathis@kombinat.at>
2022-06-28 09:40:55 +0200

    update setup.py/cfg

--- setup.py
+++ setup.py
@@ -3,36 +3,9 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "3.0.0.dev0"
-
-long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read() + "\n"
-
 setup(
+    # metadata in setup.cfg
     name="plone.app.mosaic",
-    version=version,
-    description="Plone Mosaic Site Builder and Site Layout",
-    long_description=long_description,
-    # Get more strings from
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 6.0",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords="site builder collage",
-    author="The Plone Community",
-    author_email="foundation@plone.org",
-    url="https://github.com/plone/plone.app.mosaic",
-    license="gpl",
     packages=find_packages("src"),
     package_dir={"": "src"},
     namespace_packages=["plone", "plone.app"],
@@ -40,8 +13,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "setuptools",
-        "Products.CMFPlone>=6.0.0a4",
-        "plone.subrequest",
+        "Products.CMFPlone>=6.0.0a6",
         "plone.app.blocks",
         "plone.app.tiles",
         "plone.app.drafts",
