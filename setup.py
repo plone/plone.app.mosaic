5543b489ffe93b820cd8fc68c5628ad3bf800741
Peter Mathis <peter.mathis@kombinat.at>
2025-11-17 13:48:58 +0100

    Back to development: 3.1.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 setup(
     # metadata in setup.cfg
     name="plone.app.mosaic",
-    version="3.1.6",
+    version="3.1.7.dev0",
     description="Plone Mosaic Site Builder and Site Layout",
     long_description=long_description,
     long_description_content_type="text/x-rst",
