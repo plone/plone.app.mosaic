f9b1896b1ac291eae8e65fbdb91e757edc6cbe8e
Peter Mathis <peter.mathis@kombinat.at>
2026-01-14 16:35:22 +0100

    Update Plone 6.2 dependencies and metadata.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
